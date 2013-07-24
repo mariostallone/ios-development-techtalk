39262f779e4ec890eead3f6314c8434743d44209
Mario Stallone <mariostallone@gmail.com>
2013-07-24 12:29:07 +0530

    Added a Map View Controller. Added Prototype cells and assigned the action on selection to show the view

---
+++ MapFinder/MapFinder/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  MapFinder
+//
+//  Created by Mario on 24/07/13.
+//  Copyright (c) 2013 Compassites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
