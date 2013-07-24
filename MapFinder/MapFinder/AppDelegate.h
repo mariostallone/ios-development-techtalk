66b6777f68fe25968d70af2d613dbf4a82848566
Mario Stallone <mariostallone@gmail.com>
2013-07-24 11:57:38 +0530

    New Project

---
+++ MapFinder/MapFinder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapFinder
+//
+//  Created by Mario on 24/07/13.
+//  Copyright (c) 2013 Compassites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapFinder/MapFinder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapFinder
+//
+//  Created by Mario on 24/07/13.
+//  Copyright (c) 2013 Compassites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
